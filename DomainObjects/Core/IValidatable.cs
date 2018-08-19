313c0fdf20045d57ad02fbf40f7847da1bf0d185
nsarris <nsarris@gmail.com>
2018-08-19 19:34:59 +0300

    Validation

---
+++ DomainObjects/Core/IValidatable.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainObjects.Core
+{
+    //PropertyValidator (value / collection / entity? / valueType / complex)
+    //EntityValidator
+
+    public interface IValidatable
+    {
+        void Validate();
+        //ValidationContext
+    }
+
+    public class ValidationResult
+    {
+        public bool Valid { get; private set; }
+        //List<ValidatorError> Errors
+    }
+
+    public class ValidatorResult // ValidatorError
+    {
+        //Type: Property/Entity
+        //Property
+        //Message
+        //UserMessage
+        //Code
+        //List<ValidatorResult> Inner/Nested results
+    }
+}
